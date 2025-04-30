bce4bdc02fbbf2a12c4757b69700f5af0a7fa82b
romenjivar <162660537+romenjivar@users.noreply.github.com>
2025-04-30 11:50:47 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -32,7 +32,7 @@ public class LightBoard
     for(int r = 0; r < lights.length; r++){
       if(lights[r][col]){lightsOn++;}
       if(lights[row][col] && lightsOn%2 == 0){return false;}
-      if(!lights[row][col] && lighstOn%3 == 0){return true;}
+      if(!lights[row][col] && lightsOn%3 == 0){return true;}
       return lights[row][col];
     }
  
